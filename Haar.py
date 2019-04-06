19481218302af212a1bd706fca5d56cb0fe840c7
Fandi Kurnia <m.kurniafandi@gmail.com>
2019-04-06 21:46:29 +0700

    Face Detection

---
+++ Haar.py
@@ -0,0 +1,16 @@
+import numpy as np
+import cv2
+
+face_cascade = cv2.CascadeClassifier('C:/Users/Fandi Kurnia/Documents/FaceCount/haarcascade_frontalface_alt2.xml')
+
+img = cv2.imread('C:/Users/Fandi Kurnia/Documents/FaceCount/a.jpg')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+print (len(faces))
+for (x,y,w,h) in faces:
+    cv2.rectangle(img, (x,y), (x+w, y+h), (255, 0, 0), 2)
+   
+cv2.imshow('img',img)
+cv2.waitKey()
